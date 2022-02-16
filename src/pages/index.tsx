09cf57137196c95ab78c6d48ac0ef59c0b218a97
Thiago Costa <thiagohrcosta86@gmail.com>
2022-02-16 18:54:18 -0300

    Adding new features do user form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
-import * as yup from 'yup'
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Input } from '../components/Form/Input'
 import { FieldError } from 'react-hook-form'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
 type SignInFormData = {
   email: String;
