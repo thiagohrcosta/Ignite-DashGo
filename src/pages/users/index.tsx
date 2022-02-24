7b02fbbe09ff593665ea81e7505e60a7a43e5d49
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-02-24 12:22:00 -0300

    Adding data fetching and faker

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,12 +22,19 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
+
+  useEffect(() => {
+    fetch('http:localhost:3000/api/users')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
